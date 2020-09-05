d95aea007a8823f0d3f2f1e0d7e4a93b817a34c1
pedrobethonico <66694890+pedrobethonico@users.noreply.github.com>
2020-09-05 20:32:49 -0300

    Add files via upload

---
+++ Exercício Dados Pessoais Input.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome? ")
+CPF = input ("Qual o seu CPF? ")
+idade = input ("Qual a sua idade? ")
+altura = input ("Qual a sua altura? ")
+telefone = input ("Digite seu telefone de contato! ")
+
+print (nome, "possui", idade, "e é portador do CPF", CPF,".", nome, "é um homem alto e possui", altura, "metros de altura.", "Seu telefone para contato é", telefone,".")
